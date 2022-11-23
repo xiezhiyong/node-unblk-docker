1a3106965ca32a0c7d5191291c3f63466f3635aa
zhiyong <1732244727@qq.com>
2022-11-23 15:49:00 +0800

    Create fly-deploy.sh

---
+++ fly-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+REGION="hkg"
+
+if ! command -v flyctl >/dev/null 2>&1; then
+    printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
+    curl -L https://fly.io/install.sh | FLYCTL_INSTALL=/usr/local sh
+fi
+
+if [ -z "${APP_NAME}" ]; then
+    printf '\e[31mPlease set APP_NAME first.\n\e[0m' && exit 1
+fi
+
+flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve")" = "Could not resolve" ]; then
+    printf '\e[33mCould not resolve app. Next, create the App.\n\e[0m'
+    flyctl apps create "${APP_NAME}" >/dev/null 2>&1;
+
+    flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+    if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve")" != "Could not resolve" ]; then
+        printf '\e[32mCreate app success.\n\e[0m'
+    else
+        printf '\e[31mCreate app failed.\n\e[0m' && exit 1
+    fi
+else
+    printf '\e[33mThe app has been created.\n\e[0m'
+fi
+
+printf '\e[33mNext, set app secrets and regions.\n\e[0m'
+
+flyctl regions set ${REGION}
+printf '\e[32mApp secrets and regions set success. Next, deploy the app.\n\e[0m'
+flyctl deploy --detach
+# flyctl status --app ${APP_NAME}
