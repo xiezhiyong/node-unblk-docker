ee856741d12bd6fef540eeb477e40f6b513ca473
zhiyong <1732244727@qq.com>
2022-11-23 16:54:54 +0800

    Update fly-deploy.sh

--- fly-deploy.sh
+++ fly-deploy.sh
@@ -4,7 +4,7 @@
 
 if ! command -v flyctl >/dev/null 2>&1; then
     printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
-    curl -L https://fly.io/install.sh | sh
+    curl -L https://fly.io/install.sh | FLYCTL_INSTALL=/usr/local sh
 fi
 
 if [ -z "${APP_NAME}" ]; then
